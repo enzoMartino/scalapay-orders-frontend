6d1fd16a513b266868f639d026cb01a35846052c
vmartino <enzo.martino.94@gmail.com>
2024-01-31 00:44:50 +0100

    chore: create environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    APIS_HOSTNAME: 'http://localhost',
+    APIS_PORT: 3000,
+    ORDERS_APIS_ENDPOINT: 'orders',
+    ORDERS_APIS_VERSION: 'v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    APIS_HOSTNAME: 'http://prod',
+    APIS_PORT: 3000,
+    ORDERS_APIS_ENDPOINT: 'orders',
+    ORDERS_APIS_VERSION: 'v1'
+};
