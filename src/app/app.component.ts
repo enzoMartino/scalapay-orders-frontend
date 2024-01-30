32296dcbcfa08c8e336a68a5494da3388bf52884
vmartino <enzo.martino.94@gmail.com>
2024-01-30 23:40:26 +0100

    feat: set client id cookie when starting app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CookieModule } from './common/cookies/cookies.module';
+import { CookiesService } from './common/cookies/services/cookies.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CheckoutComponent],
+  imports: [RouterOutlet, CheckoutComponent, CookieModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements AfterContentInit {
   title = 'scalapay-orders-frontend';
+
+  constructor(private readonly cookiesService: CookiesService){}
+
+  ngAfterContentInit(): void {
+    this.handleInit()
+  }
+
+  private async handleInit(): Promise<void> {
+    const clientID = await this.getClientID()
+    this.cookiesService.setClientIdCookie(clientID)
+  } 
+
+  private async getClientID(): Promise<string> {
+    return '123'
+  }
 }
