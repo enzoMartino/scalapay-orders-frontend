c38f96f3d70b7064370e426aa6a231c15a7bdf05
vmartino <enzo.martino.94@gmail.com>
2024-01-31 00:45:23 +0100

    fix: import HttpClientModule in HttpModule

--- src/app/common/http/http.module.ts
+++ src/app/common/http/http.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpService } from './services/http.service';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
   providers: [HttpService]
 })
