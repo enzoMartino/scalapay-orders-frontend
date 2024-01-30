8083f5ccc35989f179f8c39dc020b92112d3011e
vmartino <enzo.martino.94@gmail.com>
2024-01-31 00:45:45 +0100

    feat: implement placeOrder method in OrdersService

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrdersService } from './services/orders.service';
+import { HttpModule } from '../common/http/http.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpModule
+  ],
+  providers: [OrdersService]
 })
 export class OrdersModule { }
--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../../common/http/services/http.service';
+import { PlaceOrderResponseDto } from '../dtos/responses/place-order-response.dto';
+import { PlaceOrderRequestDto } from '../dtos/requests/place-order-request.dto';
+import { firstValueFrom } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class OrdersService {
+  constructor(private readonly httpService: HttpService) {}
 
-  constructor() { }
+  async placeOrder(body: PlaceOrderRequestDto): Promise<PlaceOrderResponseDto> {
+    const obs = this.httpService.post<PlaceOrderRequestDto, PlaceOrderResponseDto>('url', body)
+
+    return firstValueFrom(obs)
+  }
 }
